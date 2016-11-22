4b1c1994113b71f5ae089114e08110cabf086748
Eugene <boldenkoven@mail.ru>
2016-11-22 16:30:23 +0300

    Начал разбираться в коде

--- GEOS_3R_BIN_bin2num.m
+++ GEOS_3R_BIN_bin2num.m
@@ -1,67 +1,77 @@
 function [ OUT ] = GEOS_3R_BIN_bin2num( IN, format )
-%биты поступают от младьшего к старшему
-Input=IN;
-IN=str2num(dec2bin(IN)); % получили число в двоичном представление
- if(length(format)==length('double'))
- while(format=='double')
-    Mant=0;
-    expon=0;
-    znak=0;
-    for(k=1:6)
-        Mant=Mant+Input(k)*2^(8*(k-1));
-    end
-    Mant=Mant+mod(Input(7),2^4)*2^48;
-    Mant=Mant*2^-52;
-    expon=fix(Input(7)/2^4);
-    expon=expon+mod(Input(8),2^7)*2^4;
-    znak=fix(Input(8)/2^7);
-    OUT=(-1)^znak*(1+Mant)*2^(expon-1023);
-    break;
- end
- end
- 
- if(length(format)==length('int'))
- while(format=='int')
-    A(1:32)=0;
-    for(k=1:4) % раскладываем число по битам
-        for(m=1:8)
-            A(m+8*(k-1))=fix(mod(IN(k),10^m)/10^(m-1));
+% function [ OUT ] = GEOS_3R_BIN_bin2num( IN, format )
+%
+% ƒанна€ фунцци€ преобразует данные из символов в требуемый формат
+% ¬ходные параметры:
+%    IN     - входное число
+%    format - требуемый формат (double, int, float)
+% ¬ыходные параметры:
+%    OUT    - выходное число
+    
+% Ѕиты поступают от младшего к старшему
+    
+    Input=IN;
+    IN=str2num(dec2bin(IN)); % получили число в двоичном представление
+    if(length(format)==length('double'))
+        while(format=='double')
+            Mant=0;
+            expon=0;
+            znak=0;
+            for(k=1:6)
+                Mant=Mant+Input(k)*2^(8*(k-1));
+            end
+            Mant=Mant+mod(Input(7),2^4)*2^48;
+            Mant=Mant*2^-52;
+            expon=fix(Input(7)/2^4);
+            expon=expon+mod(Input(8),2^7)*2^4;
+            znak=fix(Input(8)/2^7);
+            OUT=(-1)^znak*(1+Mant)*2^(expon-1023);
+            break;
         end
     end
     
-    OUT=A(32); %формируем 
-    for(k=1:31)% модуль числа             модуль=сумма(ј(k)*2^(k-1)) - положительное число
-        OUT=OUT+abs(A(k)-A(32))*2^(k-1); % модуль=1+сумма(|ј(k)-1|*2^(k-1)) - отр. число
+    if(length(format)==length('int'))
+        while(format=='int')
+            A(1:32)=0;
+            for(k=1:4) % раскладываем число по битам
+                for(m=1:8)
+                    A(m+8*(k-1))=fix(mod(IN(k),10^m)/10^(m-1));
+                end
+            end
+            
+            OUT=A(32); %формируем 
+            for(k=1:31)% модуль числа             модуль=сумма(ј(k)*2^(k-1)) - положительное число
+                OUT=OUT+abs(A(k)-A(32))*2^(k-1); % модуль=1+сумма(|ј(k)-1|*2^(k-1)) - отр. число
+            end
+            OUT=((-1)^A(32))*OUT; %учитываем знак
+            
+            break;
+        end
     end
-    OUT=((-1)^A(32))*OUT; %учитываем знак
-    
-    break;
- end
- end
 
- if(length(format)==length('float'))
- while(format=='float')
-    A(1:32)=0;
-    for(k=0:3) % раскладываем число по битам
-        for(m=1:8)
-            A(m+8*k)=fix(mod(IN(k+1),10^m)/10^(m-1));
+    if(length(format)==length('float'))
+        while(format=='float')
+            A(1:32)=0;
+            for(k=0:3) % раскладываем число по битам
+                for(m=1:8)
+                    A(m+8*k)=fix(mod(IN(k+1),10^m)/10^(m-1));
+                end
+            end
+            
+            Man=0; %мантисса
+            for(k=1:23)
+                Man=Man+A(24-k)*2^(-k);
+            end
+            
+            expon=0; % пор€док
+            for(k=1:8)
+                expon=expon+A(23+k)*2^(k-1);
+            end
+            
+            OUT=(-1)^(A(32))*(1+Man)*2^(expon-127);
+            break;
         end
     end
     
-    Man=0; %мантисса
-    for(k=1:23)
-        Man=Man+A(24-k)*2^(-k);
-    end
-    
-    expon=0; % пор€док
-    for(k=1:8)
-        expon=expon+A(23+k)*2^(k-1);
-    end
-    
-    OUT=(-1)^(A(32))*(1+Man)*2^(expon-127);
- break;
- end
- end
- 
 end
 
--- GEOS_3R_BIN_main.m
+++ GEOS_3R_BIN_main.m
@@ -3,196 +3,214 @@ clc
 
 %fclose(instrfind)
 
-fead_data_from='GEOS';
+%fead_data_from='GEOS';
 fead_data_from='file';
 
-if(fead_data_from=='GEOS')
-    %настройки COM порта
-    Num_com_port='COM67';
-    baud=115200;
-    com_port=serial(Num_com_port, 'BaudRate', baud);
-else
-    name_bin_file='bin_data_file\geos_3MR_F_1_Hz.bin';
-    com_port=fopen(name_bin_file);
+if (fead_data_from=='GEOS')      % Настройки из порта
+    Num_com_port = 'COM67';   
+    baud = 115200;
+    stream = serial(Num_com_port, 'BaudRate', baud);
+else                             % Работа из файла
+    name_bin_file = 'bin_data_file/geos_3MR_F_1_Hz.bin';
+    stream = fopen(name_bin_file, 'r');
 end
 
 
-%что обрабатывать
-phData.NumPointPlot=30; % максимальное число точек на графике
-phData.NumPointPoiskKA=5;    % число точек для поиска нужного КА
-phData.chisl_spytnicov=96;%  число спутников 
+% Что обрабатывать
+phData.NumPointPlot=30;          % Максимальное число точек на графике
+phData.NumPointPoiskKA=5;        % Число точек для поиска нужного КА
+phData.chisl_spytnicov=96;       % Число спутников 
 
-%константы:
-F0_gps=1.57542e9; % центральная частота gps
-F0_gln=1.602e9; % центральная частота ГЛОНАСС
-dF_gln=562500; %шаг по частоте ГЛОНАСС (ЧРК, литеры)
-c=299792458; % скорость света
+% Константы:
+F0_gps=1.57542e9;                % Центральная частота gps
+F0_gln=1.602e9;                  % Центральная частота ГЛОНАСС
+dF_gln=562500;                   % Шаг по частоте ГЛОНАСС (ЧРК, литеры)
+c=299792458;                     % Скорость света
 
-phData.reliable(1:phData.NumPointPlot,1:phData.chisl_spytnicov)=nan; 
-phData.SignalToNoise(1:phData.NumPointPoiskKA,1:phData.chisl_spytnicov)=nan;
-phData.phase(1:phData.NumPointPlot,1)=nan;
-phData.time(1:phData.NumPointPlot,1)=nan;
-phData.H_liter=nan;
-phData.cycle_d(1:phData.NumPointPlot,1)=nan;
-t_gps(1:phData.NumPointPlot,1)=nan;
-% Bin.preamble=[hex2dec('47'); hex2dec('45'); hex2dec('4f'); hex2dec('53'); ...
-%     hex2dec('72'); hex2dec('33'); hex2dec('50'); hex2dec('53')]; %преамбула GEOSr3PS
+% Выделение памяти под массивы для данных
+phData.reliable(1:phData.NumPointPlot,1:phData.chisl_spytnicov) = nan; 
+phData.SignalToNoise(1:phData.NumPointPoiskKA,1:phData.chisl_spytnicov) = nan;
+phData.phase(1:phData.NumPointPlot,1) = nan;
+phData.time(1:phData.NumPointPlot,1) = nan;
+phData.H_liter = nan;
+phData.cycle_d(1:phData.NumPointPlot,1) = nan;
+t_gps(1:phData.NumPointPlot,1) = nan;
 
-if(fead_data_from=='GEOS') %для прм GEOS-3R
-    %настройки бинарного протокола приемника
-    % выдача сообщений 0х10 и 0x13
+if(fead_data_from=='GEOS')       % Для прм GEOS-3R
+                                 % Настройки бинарного протокола приемника
+                                 % Выдача сообщений 0х10 и 0x13
+
+    % Отправка пакета 0x4F - разрешение бинарных сообщений
     Bin.data_write(1:8,1)=[hex2dec('4F'); 0; 1; 0; 0; 0; 9; 0];
-    GEOS_3R_BIN_DataWrite(Bin.data_write, com_port);
+    GEOS_3R_BIN_DataWrite(Bin.data_write, stream);
     
-    % темп выдачи (1 Гц)
+    % Отправка пакета 0x44 - темп выдачи (1 Гц)
     Bin.data_write(1:8,1)=[hex2dec('44'); 0; 1; 0; 3; 0; 0; 0];
-    GEOS_3R_BIN_DataWrite(Bin.data_write, com_port);
+    GEOS_3R_BIN_DataWrite(Bin.data_write, stream);
     
-    fopen(com_port);
+    fopen(stream);
 end
 
 StartTime=-1; % время начала работы
 
-while(1==1) % определяем номер КА
-    for(cikl_for=1:phData.NumPointPoiskKA)
-        %считывание даннных из порта
+while (1)    % Выбираем КА, с которым дальше будем работать
+    for ch=1:phData.NumPointPoiskKA
+        % Считывание пакета 16 (0x10) - измерительная информация каналов 
         
-        [Bin.PH_data, phData.datN]=GEOS_3R_BIN_DataRead(16,com_port);
+        [Bin.PH_data, phData.datN] = GEOS_3R_BIN_DataRead(16, stream);
 
+        % Преобразование слова "Количество КА"
+        phData.NumKA=GEOS_3R_BIN_bin2num(Bin.PH_data(13:16,1), 'int');
+        fprintf('NUM KA: %d  \n', phData.NumKA)
         
-        while(1==1)% выбор спутника. критерий- макс. с/ш
-            phData.NumKA=GEOS_3R_BIN_bin2num(Bin.PH_data(13:16,1),'int');
-            fprintf('NUM KA: %d  \n', phData.NumKA) %print число КА в решение
-            for(k=1:phData.NumKA)%обработка данных с "к"-го спутника
-                [phData.tmp_KAnumber, phData.tmp_reliable, phData.tmp_SignalToNoise,...
-                    phData.tmp_phase,phData.tmp_Doppler]=GEOS_3R_BIN_DataDecod_0x10(...
-                    Bin.PH_data(4*(-9+14*k)-3:4*(4+14*k),1));
-                phData.SignalToNoise(cikl_for,phData.tmp_KAnumber)=phData.tmp_SignalToNoise;
-            end
-            break
+        for k=1:phData.NumKA      % Обработка всех спутников из пакета 0x10
+
+            % Номера байт, отвещающих за спутник k в пакете 0x10
+            rec_ind = 4*(-9+14*k)-3:4*(4+14*k);
+
+            % Выделение измерений одно КА из пакета 0x10
+            [phData.tmp_KAnumber        ...   % Номер НС
+             phData.tmp_reliable        ...   % Флаги достоверности
+             phData.tmp_SignalToNoise   ...   % q, дБГц
+             phData.tmp_phase           ...   % Фаза
+             phData.tmp_Doppler     ] = ...   % Смещение частоты
+                GEOS_3R_BIN_DataDecod_0x10( Bin.PH_data(rec_ind, 1) );
+            
+            % Заполнение массива отношением сигнал/шум
+            phData.SignalToNoise(ch, phData.tmp_KAnumber) = phData.tmp_SignalToNoise;
         end
         
     end
-    %условия выхода из цикла выбора КА
-   [logik,ind_KA]=max(sum(phData.SignalToNoise));%поиск максимального отношения с/ш
-   if(logik>40*phData.NumPointPoiskKA) % для завершения поиска требуется отношение с/ш больше 40 dBHz
-       fprintf('\n ok \n num KA= %d \n max signalTOnoise %f \n', ind_KA, logik/phData.NumPointPoiskKA)
-       break
-   else
-       fprintf('\n not \n max signalTOnoise= %f \n', logik/phData.NumPointPoiskKA)
-   end
+    
+    % условия выхода из цикла выбора КА
+    [snr, ind_KA] = max(mean(phData.SignalToNoise));  % Поиск максимального отношения с/ш
+    if(snr > 40)                                      % Для завершения поиска требуется отношение с/ш больше 40 dBHz
+        fprintf('\n OK:  best KA is %d,    SNR = %5.1f dBHz\n', ind_KA, snr)
+        break
+    else
+        fprintf('\n Failed: max SNR = %5.1f dBHz\n', snr)
+    end
 end
-fclose(com_port);
-%закончили поиск
-phData.SignalToNoise=nan;
-phData.SignalToNoise(1:phData.NumPointPlot,1)=nan;
+fclose(stream);  % Поиск подходящего КА завершён
 
-%phData.k=3.369036432266235e+003; %это можно удалять
-%phData.o= 4.208831474092468e-004;
-%1.603687500000000e+009;
+% Выделение памяти для данных
+phData.SignalToNoise = nan;
+phData.SignalToNoise(1:phData.NumPointPlot, 1) = nan;
 
-if(fead_data_from=='GEOS')
-    fopen(com_port);
+if (fead_data_from=='GEOS')
+    fopen(stream);
 else
-    com_port=fopen(name_bin_file);
+    stream=fopen(name_bin_file);
 end
 
-while(1==1) % обработка данных с заданного КА
-%    fclose(com_port);
-%    clc
-%    fopen(com_port);
-%    for(open_close_port=1:33)
-        [Bin.PH_data,phData.datN]=GEOS_3R_BIN_DataRead(16,com_port);
-        [Bin.navi_task,tmp]=GEOS_3R_BIN_DataRead(hex2dec('13'),com_port);
+while (1) % обработка данных с заданного КА
+            %    fclose(stream);
+            %    clc
+            %    fopen(stream);
+            %    for(open_close_port=1:33)
+    
+    % Ожидание пакета 16 (0x10) - Измерительная информация каналов
+    [Bin.PH_data, phData.datN]=GEOS_3R_BIN_DataRead(hex2dec('10'), stream);
+    
+    % Ожидание пакета 19 (0x13) - Вектор состояния НЗ
+    [Bin.navi_task, tmp]=GEOS_3R_BIN_DataRead(hex2dec('13'), stream);
+    
+    while (1)  % Обработка сообщений 0х10 и 0х13
+        fprintf('\n ____________0x10___________')
         
-        while(1==1) %обработка сообщений 0х10 и 0х13
-            fprintf('\n ____________0x10___________')
-            
-            for(m=1:phData.NumPointPlot-1)% смещаемся на такт вниз
-                phData.phase(phData.NumPointPlot-m+1,1)=phData.phase(phData.NumPointPlot-m,1);
-                phData.SignalToNoise(phData.NumPointPlot-m+1,1)=phData.SignalToNoise(phData.NumPointPlot-m,1);
-                phData.reliable(phData.NumPointPlot-m+1,1)=phData.reliable(phData.NumPointPlot-m,1);
-                phData.time(phData.NumPointPlot-m+1,1)=phData.time(phData.NumPointPlot-m,1);
-                phData.cycle_d(phData.NumPointPlot-m+1,1)=phData.cycle_d(phData.NumPointPlot-m,1);
-                t_gps(phData.NumPointPlot-m+1,1)=t_gps(phData.NumPointPlot-m,1);%0x13
-            end
-            
-            [ phData.UTC, phData.cycle_d(1,1),  phData.kol_vo_KA, phData.SignalToNoise(1),...
-                phData.phase(1),  phData.Doppler, phData.H_liter]=...
-                GEOS_3R_BIN_KA_data_0x10( Bin.PH_data,  ind_KA);
-            if(StartTime<0) %задаём начальное время (для графиков)
-                StartTime=phData.UTC;
-                %phData.b=phData.phase(1,1)+phData.k*phData.o*(16369003-phData.cycle_d(1,1));%удалить
-                %частота сигнала:
-                if(ind_KA<65)
-                    F_signal=F0_gps;
-                else
-                    F_signal=F0_gln+phData.H_liter*dF_gln;
-                end
-            end
-           phData.time(1,1)=(phData.UTC-StartTime); %по НС определяем
-%            MEAN = mean(phData.cycle_d);
-%            ind = find(abs(phData.cycle_d-MEAN)<0.5);
-%            phData.mean_Fd=mean(phData.cycle_d(ind));
-%            phData.TIME= phData.time+(phData.cycle_d(1,1)-phData.mean_Fd)/phData.mean_Fd;
-           
-           fprintf('\n UTC (01.01.2008): %9.0f sec', phData.UTC);
-            fprintf('\n number KA: %d', phData.kol_vo_KA);
-            t_gps(1,1)=GEOS_3R_BIN_navi_task_0x13(Bin.navi_task);%0x13
-            break;
+        for m=1:phData.NumPointPlot-1     % смещаемся на такт вниз
+            phData.phase(phData.NumPointPlot-m+1,1)=phData.phase(phData.NumPointPlot-m,1);
+            phData.SignalToNoise(phData.NumPointPlot-m+1,1)=phData.SignalToNoise(phData.NumPointPlot-m,1);
+            phData.reliable(phData.NumPointPlot-m+1,1)=phData.reliable(phData.NumPointPlot-m,1);
+            phData.time(phData.NumPointPlot-m+1,1)=phData.time(phData.NumPointPlot-m,1);
+            phData.cycle_d(phData.NumPointPlot-m+1,1)=phData.cycle_d(phData.NumPointPlot-m,1);
+            t_gps(phData.NumPointPlot-m+1,1)=t_gps(phData.NumPointPlot-m,1);%0x13
         end
+        
+        % Выдение данных для требуемого КА
+        [ phData.UTC,               ...   % Время UTC с 1 января 2008 года (стандартное для Геос)
+          phData.cycle_d(1,1),      ...   % Количество тактов АЦП на текущем измерительном интервале
+          phData.kol_vo_KA,         ...   % Количество КА в пакете 0x10
+          phData.SignalToNoise(1),  ...   % Отношение сигнал/шум
+          phData.phase(1),          ...   % Фаза
+          phData.Doppler,           ...   % Смещение частоты
+          phData.H_liter        ] = ...   % ?
+            GEOS_3R_BIN_KA_data_0x10( Bin.PH_data,  ind_KA);
+        
+        if (StartTime<0)                  % Задаём начальное время (для графиков)
+            StartTime = phData.UTC;
 
-        %обработка данных:
-        if(sum(~isnan(phData.phase))>3)
-            k=find(isnan(phData.phase)==0);
-            apr.phase=phData.phase(k);
-            apr.time=phData.time(k);
-            apr.p2=polyfit(apr.time,apr.phase,2);
-            apr.DataFit = polyval(apr.p2,apr.time);
-            apr.DataPhase2=(apr.phase-apr.DataFit);
-            tmp=0;
-            m=0;
-            for(k=1:length(apr.DataPhase2))
-                if(isnan(apr.DataPhase2)==0)
-                    tmp=tmp+apr.DataPhase2(k);
-                    m=m+1;
-                end
+            if (ind_KA < 65)              % Несущая частота сигнала
+                F_signal = F0_gps;
+            else
+                F_signal = F0_gln+phData.H_liter*dF_gln;
             end
-            apr.sredn(1:length(apr.DataPhase2))=tmp/m;
-        else
-            apr.DataPhase2=nan;
-            apr.DataFit=nan;
-            apr.time=nan;
-            apr.aprTime=nan;
-            apr.aprDataPhase2=nan;
-            apr.sredn=nan;
-        end       
+        end
         
-        %вывод графиков:
-        subplot(2,2,1)
-        plot(phData.time, phData.cycle_d,'r')
-        xlabel('time, sec');
-        ylabel('phase, cycles');
-        subplot(2,2,2)
-        plot(phData.time,phData.SignalToNoise)
-        xlabel('time, sec');
-        ylabel('signal to noise, dBHz');
-        subplot(2,2,3)
-        plot(apr.time,apr.DataPhase2,'b')
-        xlabel('time, sec');
-        ylabel('d phase, cycles');
-        subplot(2,2,4)
-        plot(phData.time,phData.phase, 'b',apr.time, apr.DataFit,'r')
-        xlabel('time, sec');
-        ylabel('d phase, ');
-        drawnow
-%    end
+        phData.time(1,1) = (phData.UTC-StartTime); % по НС определяем
+                                                   %            MEAN = mean(phData.cycle_d);
+                                                   %            ind = find(abs(phData.cycle_d-MEAN)<0.5);
+                                                   %            phData.mean_Fd=mean(phData.cycle_d(ind));
+                                                   %            phData.TIME= phData.time+(phData.cycle_d(1,1)-phData.mean_Fd)/phData.mean_Fd;
+        
+        fprintf('\n UTC (01.01.2008): %9.0f sec', phData.UTC);
+        fprintf('\n number KA: %d', phData.kol_vo_KA);
+        
+        t_gps(1,1) = GEOS_3R_BIN_navi_task_0x13(Bin.navi_task);%0x13
+        break;
+    end
+
+    %обработка данных:
+    if(sum(~isnan(phData.phase))>3)
+        k=find(isnan(phData.phase)==0);
+        apr.phase=phData.phase(k);
+        apr.time=phData.time(k);
+        apr.p2=polyfit(apr.time,apr.phase,2);
+        apr.DataFit = polyval(apr.p2,apr.time);
+        apr.DataPhase2=(apr.phase-apr.DataFit);
+        tmp=0;
+        m=0;
+        for(k=1:length(apr.DataPhase2))
+            if(isnan(apr.DataPhase2)==0)
+                tmp=tmp+apr.DataPhase2(k);
+                m=m+1;
+            end
+        end
+        apr.sredn(1:length(apr.DataPhase2))=tmp/m;
+    else
+        apr.DataPhase2=nan;
+        apr.DataFit=nan;
+        apr.time=nan;
+        apr.aprTime=nan;
+        apr.aprDataPhase2=nan;
+        apr.sredn=nan;
+    end       
+    
+    %вывод графиков:
+    subplot(2,2,1)
+    plot(phData.time, phData.cycle_d,'r')
+    xlabel('time, sec');
+    ylabel('phase, cycles');
+    subplot(2,2,2)
+    plot(phData.time,phData.SignalToNoise)
+    xlabel('time, sec');
+    ylabel('signal to noise, dBHz');
+    subplot(2,2,3)
+    plot(apr.time,apr.DataPhase2,'b')
+    xlabel('time, sec');
+    ylabel('d phase, cycles');
+    subplot(2,2,4)
+    plot(phData.time,phData.phase, 'b',apr.time, apr.DataFit,'r')
+    xlabel('time, sec');
+    ylabel('d phase, ');
+    drawnow
+    %    end
 end
-    
-        
-        
-        
-       
-    
+
+
+
+
+
+
 
 
