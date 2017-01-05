69e9a6547d28f0815c09a7a52dd5d6e30b4faa4f
Eugene <boldenkoven@mail.ru>
2017-01-05 14:06:03 +0300

    Переделал выделение float по аналогии с double

--- bin2num.m
+++ bin2num.m
@@ -4,7 +4,7 @@ function [ OUT ] = bin2num( IN, format )
 % ƒанна€ функци€ преобразует данные из символов в требуемый формат
 % ¬ходные параметры:
 %    IN     - входное число
-%    format - требуемый формат (double, int, float)
+%    format - требуемый формат (double, float, int8, uint8, ..., uint32)
 % ¬ыходные параметры:
 %    OUT    - выходное число
     
@@ -14,40 +14,39 @@ function [ OUT ] = bin2num( IN, format )
     
     if strcmp(format, 'double')
         Input=IN;
-        IN=str2num(dec2bin(IN)); % получили число в двоичном представлении
 
         Mant=0;
         expon=0;
         znak=0;
-        for(k=1:6)
+        for k=1:6
             Mant=Mant+Input(k)*2^(8*(k-1));
         end
         Mant=Mant+mod(Input(7),2^4)*2^48;
         Mant=Mant*2^-52;
+        
         expon=fix(Input(7)/2^4);
         expon=expon+mod(Input(8),2^7)*2^4;
+        
         znak=fix(Input(8)/2^7);
         OUT=(-1)^znak*(1+Mant)*2^(expon-1023);
     
     elseif strcmp(format, 'float')
-        A(1:32)=0;
-        for(k=0:3) % раскладываем число по битам
-            for(m=1:8)
-                A(m+8*k)=fix(mod(IN(k+1),10^m)/10^(m-1));
-            end
+        Input=IN;
+
+        Mant=0;
+        expon=0;
+        znak=0;
+        for k=1:2
+            Mant=Mant+Input(k)*2^(8*(k-1));
         end
+        Mant=Mant+mod(Input(3),2^7)*2^16;
+        Mant=Mant*2^-23;
         
-        Man=0; %мантисса
-        for(k=1:23)
-            Man=Man+A(24-k)*2^(-k);
-        end
+        expon=fix(Input(3)/2^7);
+        expon=expon+mod(Input(4),2^7)*2^1;
         
-        expon=0; % пор€док
-        for(k=1:8)
-            expon=expon+A(23+k)*2^(k-1);
-        end
-        
-        OUT=(-1)^(A(32))*(1+Man)*2^(expon-127);
+        znak=fix(Input(4)/2^7);
+        OUT=(-1)^znak*(1+Mant)*2^(expon-127);
         
     elseif strcmp(format, 'int8') || strcmp(format, 'int16') || strcmp(format, 'int32')
         OUT = 0;
@@ -65,9 +64,6 @@ function [ OUT ] = bin2num( IN, format )
             OUT = OUT + IN(i)*2^(8*(i-1));
         end
     end
-    
-
-
     
 end
 
